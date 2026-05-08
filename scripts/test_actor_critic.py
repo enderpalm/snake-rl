0754d6b034258d85b4c42c80794686c223301405
Knightrider8611 <rathanon.makaramanee@gmail.com>
2026-05-08 22:21:02 +0700

    Renders ui for actor critic and switch seeds.

--- scripts/test_actor_critic.py
+++ scripts/test_actor_critic.py
@@ -1,5 +1,6 @@
 from agents.actor_critic import ActorCriticAgent
 from core.env.core import RewardOptions
+from core.env.types import RenderMode, RenderOptions
 from core.utils import evaluate_agent
 
 
@@ -10,12 +11,14 @@ def run_test():
 
     evaluate_agent(
         ac_agent,
-        num_episodes=1000,
+        num_episodes=3,
         num_apples=(1, 3),
         num_obstacles=(5, 12),
         seed=67,
         num_envs=16,
         max_steps=10000,
+        render_mode=RenderMode.HUMAN,
+        render_options=RenderOptions(agent_color=(255, 197, 211)),
         reward_options=RewardOptions(
             eats_apple=24.0,
             penalty_step=-0.01,
