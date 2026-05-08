c3d4f901d5b413dfdf6da0380c714fcab8f64a69
Knightrider8611 <rathanon.makaramanee@gmail.com>
2026-05-08 23:14:32 +0700

    Changed number of episodes used to test and changed all seed to 42. Make envionment all the same

--- scripts/test_actor_critic.py
+++ scripts/test_actor_critic.py
@@ -5,16 +5,16 @@ from core.utils import evaluate_agent
 
 
 def run_test():
-    ac_agent = ActorCriticAgent(state_dim=11, action_dim=3, seed=67)
+    ac_agent = ActorCriticAgent(state_dim=11, action_dim=3, seed=42)
 
     ac_agent.load("./artifacts/models/actor_critic_snake_best.pth")
 
     evaluate_agent(
         ac_agent,
-        num_episodes=3,
-        num_apples=(1, 3),
-        num_obstacles=(5, 12),
-        seed=67,
+        num_episodes=20,
+        num_apples=2,
+        num_obstacles=10,
+        seed=42,
         num_envs=16,
         max_steps=10000,
         render_mode=RenderMode.HUMAN,
--- scripts/test_q_learning.py
+++ scripts/test_q_learning.py
@@ -10,8 +10,8 @@ def run_test():
         agent,
         num_obstacles=10,
         num_apples=2,
-        num_episodes=3,
-        render_mode=RenderMode.HUMAN,
+        num_episodes=20,
+        render_mode=None,
         seed=42,
         render_options=RenderOptions(agent_color=(190, 128, 250)),
     )
